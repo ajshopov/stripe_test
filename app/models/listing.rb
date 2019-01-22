1dbc68aafa2f1ab53beee17cca3fb84d293eb65c
ajshopov <alexanderjshopov@gmail.com>
2019-01-22 15:38:09 +1100

    set up and link Order model and new order form

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,5 @@ class Listing < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
 
   belongs_to :user
+  has_many :orders
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_205318) do
+ActiveRecord::Schema.define(version: 2019_01_22_015929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 2019_01_18_205318) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "listing_id"
+    t.integer "buyer_id"
+    t.integer "seller_id"
   end
 
   create_table "users", force: :cascade do |t|
